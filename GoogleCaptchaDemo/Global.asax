7bfb04ea3100662132c2b8c7a22128d86cdb77eb
thiqahdev\a.aabouelenein <aabouelenein@THQ1LTAABOUELEN>
2020-01-09 13:29:36 +0300

    initial commit

---
+++ GoogleCaptchaDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoogleCaptchaDemo.MvcApplication" Language="C#" %>
---
+++ GoogleCaptchaDemo/Helpers/GoogleCaptchaHelper.cs
@@ -0,0 +1,37 @@
+﻿using GoogleCaptchaDemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace GoogleCaptchaDemo.Helpers
+{
+    public class GoogleCaptchaHelper
+    {
+        public static bool ValdiateCaptcha(string encodedResponse)
+        {
+            if (String.IsNullOrEmpty(encodedResponse))
+                return false;
+
+
+            var googleRecaptchaUrl = ConfigurationManager.AppSettings["GoogleRecaptchaUrl"];
+            var googleRecaptchaSecret = ConfigurationManager.AppSettings["GoogleRecaptchaSecret"];
+            var client = new WebClient();
+            var reply =
+                client.DownloadString(
+                    string.Format(googleRecaptchaUrl, googleRecaptchaSecret, encodedResponse));
+
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponseModel>(reply);
+
+            if (!captchaResponse.Success)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
